0da365a9008519a72755b9667277b41b65794d1f
jwhaosis <jwhaosis@gmail.com>
2018-03-20 18:33:27 -0400

    blanks

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ get '/' do
   redirect "http://scuteser.herokuapp.com"
 end
 
+get '/blank' do
+  erb :master
+end
+
+get '/test' do
+
+end
+
 get '/loaderio-db0de517c7188e5b1e035c98a0e33e25/' do
   "loaderio-db0de517c7188e5b1e035c98a0e33e25"
 end
