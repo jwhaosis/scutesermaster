d4edfb5026e5b0a28d3532d0c1dc50954593db35
jwhaosis <jwhaosis@gmail.com>
2018-03-20 17:52:37 -0400

    Blarg

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ get '/' do
   redirect "http://scuteser.herokuapp.com"
 end
 
-get '/loaderio-db0de517c7188e5b1e035c98a0e33e25/'
+get '/loaderio-db0de517c7188e5b1e035c98a0e33e25/' do
   "loaderio-db0de517c7188e5b1e035c98a0e33e25"
 end
