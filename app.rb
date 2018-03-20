f26e8be5b70e11ea03a962153e1f103cf3a83443
jwhaosis <jwhaosis@gmail.com>
2018-03-20 17:50:41 -0400

    loader

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require 'sinatra'
 get '/' do
   redirect "http://scuteser.herokuapp.com"
 end
+
+get '/loaderio-db0de517c7188e5b1e035c98a0e33e25/'
+  "loaderio-db0de517c7188e5b1e035c98a0e33e25"
+end
