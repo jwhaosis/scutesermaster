7c018bdcfcf5780232118399b389fef71c05a9f6
jwhaosis <jwhaosis@gmail.com>
2018-04-17 12:51:23 -0400

    loader again

--- app.rb
+++ app.rb
@@ -8,12 +8,26 @@ uri = URI.parse(ENV["REDIS_URI"])
 redis = Redis.new(:host => uri, :port => 10619, :password => ENV["REDIS_PASS"])
 redis.setnx "loadc", "100"
 first_uri = 'http://scuteser'
-second_uri = '.herokuapp.com/'
 
-after do
+get '/loaderio-87117f3cc6f3476f7ec8e1f03770d4f8/' do
+  'loaderio-87117f3cc6f3476f7ec8e1f03770d4f8'
+end
+
+get '/' do
+ second_uri = '.herokuapp.com/'
+ redis.incr "loadc"
+ target = 1 + (redis.get "loadc").to_i%4
+ if target == 1
+   redirect first_uri + second_uri
+ else
+   redirect first_uri + "-#{target}" + second_uri
+ end
+end
+
+get '/:path1' do
+  second_uri = '.herokuapp.com/' + params[:path1]
   redis.incr "loadc"
   target = 1 + (redis.get "loadc").to_i%4
-
   if target == 1
     redirect first_uri + second_uri
   else
@@ -21,30 +35,24 @@ after do
   end
 end
 
-get '/' do
-
-end
-
-get '/:path1' do
-  second_uri += params[:path1]
-end
-
-get '/:path1/' do
-  second_uri += params[:path1]
-end
-
 get '/:path1/:path2' do
-  second_uri = second_uri + params[:path1] + '/' + params[:path2]
-end
-
-get '/:path1/:path2/' do
-  second_uri = second_uri + params[:path1] + '/' + params[:path2]
+  second_uri = '.herokuapp.com/' + params[:path1] + '/' + params[:path2]
+  redis.incr "loadc"
+  target = 1 + (redis.get "loadc").to_i%4
+  if target == 1
+    redirect first_uri + second_uri
+  else
+    redirect first_uri + "-#{target}" + second_uri
+  end
 end
 
 get '/:path1/:path2/:path3' do
-  second_uri = second_uri + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
-end
-
-get '/:path1/:path2/:path3/' do
-  second_uri = second_uri + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
+  second_uri = '.herokuapp.com/' + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
+  redis.incr "loadc"
+  target = 1 + (redis.get "loadc").to_i%4
+  if target == 1
+    redirect first_uri + second_uri
+  else
+    redirect first_uri + "-#{target}" + second_uri
+  end
 end
