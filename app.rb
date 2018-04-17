13913320375cb3e6dd009993a2186b9a079f8b28
jwhaosis <jwhaosis@gmail.com>
2018-04-17 11:47:26 -0400

    balance

--- app.rb
+++ app.rb
@@ -7,35 +7,34 @@ Dotenv.load
 uri = URI.parse(ENV["REDIS_URI"])
 redis = Redis.new(:host => uri, :port => 10619, :password => ENV["REDIS_PASS"])
 redis.setnx "loadc", "100"
+first_uri = 'http://scuteser'
+second_uri = '.herokuapp.com/'
 
 after do
   redis.incr "loadc"
-  target = (redis.get "loadc").to_i%5
-  if target == 0
-    redirect 'http://scuteser.herokuapp.com/'
-  elsif target == 1
-    redirect 'http://scuteser-2.herokuapp.com/'
-  elsif target == 2
-    redirect 'http://scuteser-3.herokuapp.com/'
-  elsif target == 3
-    redirect 'http://scuteser-4.herokuapp.com/'
+  uri = first_uri + second_uri
+  target = 1 + (redis.get "loadc").to_i%4
+  if target == 1
+    redirect first_uri + second_uri
+  elsif
+    redirect first_uri + "-#{target}" + second_uri
   end
 end
 
 get '/' do
-  
+
 end
 
 get '/:path1' do
-
+  second_uri += params[:path1]
 end
 
 get '/:path1/:path2' do
-
+  second_uri = second_uri + params[:path1] + '/' + params[:path2]
 end
 
 get '/:path1/:path2/:path3' do
-
+  second_uri = second_uri + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
 end
 
 get '/loaderio-87117f3cc6f3476f7ec8e1f03770d4f8/' do
