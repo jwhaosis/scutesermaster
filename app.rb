5c13a4a33718ed20801cf9e1af889d04b130730b
jwhaosis <jwhaosis@gmail.com>
2018-04-17 12:26:03 -0400

    loader fix

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ second_uri = '.herokuapp.com/'
 
 after do
   redis.incr "loadc"
-  uri = first_uri + second_uri
   target = 1 + (redis.get "loadc").to_i%4
-  if target == 1
+  if second_uri.include? "loaderio"
+    send_file "loader.html"
+  elsif target == 1
     redirect first_uri + second_uri
-  elsif
+  else
     redirect first_uri + "-#{target}" + second_uri
   end
 end
@@ -29,7 +30,15 @@ get '/:path1' do
   second_uri += params[:path1]
 end
 
+get '/:path1/' do
+  second_uri += params[:path1]
+end
+
 get '/:path1/:path2' do
+  second_uri = second_uri + params[:path1] + '/' + params[:path2]
+end
+
+get '/:path1/:path2/' do
   second_uri = second_uri + params[:path1] + '/' + params[:path2]
 end
 
@@ -37,10 +46,6 @@ get '/:path1/:path2/:path3' do
   second_uri = second_uri + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
 end
 
-get '/loaderio-87117f3cc6f3476f7ec8e1f03770d4f8/' do
-  "loaderio-87117f3cc6f3476f7ec8e1f03770d4f8"
-end
-
-get '/loaderio-db0de517c7188e5b1e035c98a0e33e25/' do
-  "loaderio-db0de517c7188e5b1e035c98a0e33e25"
+get '/:path1/:path2/:path3/' do
+  second_uri = second_uri + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
 end
