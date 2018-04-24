0777ceb9a7a2ce052ad042ce679ac7b0d761d25e
jwhaosis <jwhaosis@gmail.com>
2018-04-24 01:24:10 -0400

    array

--- app.rb
+++ app.rb
@@ -6,52 +6,37 @@ require 'dotenv'
 Dotenv.load
 $redis = Redis.new(:host => ENV["REDIS_URI"], :port => 10619, :password => ENV["REDIS_PASS"])
 $redis.setnx "loadc", "100"
-first_uri = 'http://scuteser'
+urls = ["https://scuteser.herokuapp.com","https://scuteser-2.herokuapp.com","https://scuteser-3.herokuapp.com","https://scuteser-4.herokuapp.com"]
 
 get '/loaderio-87117f3cc6f3476f7ec8e1f03770d4f8/' do
   'loaderio-87117f3cc6f3476f7ec8e1f03770d4f8'
 end
 
 get '/' do
- second_uri = '.herokuapp.com/'
  $redis.incr "loadc"
- target = 1 + ($redis.get "loadc").to_i%4
- if target == 1
-   redirect first_uri + second_uri
- else
-   redirect first_uri + "-#{target}" + second_uri
- end
+ redirect urls[($redis.get "loadc").to_i%4]
+end
+
+get '/test/reset/all' do
+  EM.run {
+    urls.each do |url|
+      request = EM::HttpRequest.new("#{url}/test/reset/all").post
+    end
+    EM.stop
+  }
 end
 
 get '/:path1' do
-  second_uri = '.herokuapp.com/' + params[:path1]
   $redis.incr "loadc"
-  target = 1 + ($redis.get "loadc").to_i%4
-  if target == 1
-    redirect first_uri + second_uri
-  else
-    redirect first_uri + "-#{target}" + second_uri
-  end
+  redirect "#{urls[($redis.get "loadc").to_i%4]}/#{params[:path1]}"
 end
 
 get '/:path1/:path2' do
-  second_uri = '.herokuapp.com/' + params[:path1] + '/' + params[:path2]
   $redis.incr "loadc"
-  target = 1 + ($redis.get "loadc").to_i%4
-  if target == 1
-    redirect first_uri + second_uri
-  else
-    redirect first_uri + "-#{target}" + second_uri
-  end
+  redirect "#{urls[($redis.get "loadc").to_i%4]}/#{params[:path1]}/#{params[:path2]}"
 end
 
 get '/:path1/:path2/:path3' do
-  second_uri = '.herokuapp.com/' + params[:path1] + '/' + params[:path2] + '/' + params[:path3]
   $redis.incr "loadc"
-  target = 1 + ($redis.get "loadc").to_i%4
-  if target == 1
-    redirect first_uri + second_uri
-  else
-    redirect first_uri + "-#{target}" + second_uri
-  end
+  redirect "#{urls[($redis.get "loadc").to_i%4]}/#{params[:path1]}/#{params[:path2]}/#{params[:path3]}"
 end
